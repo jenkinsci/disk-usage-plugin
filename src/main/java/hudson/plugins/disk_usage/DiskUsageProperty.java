07e1068aa1a002a17f33923e6033bb65e1802930
lvotypko <lvotypko@redhat.com>
2013-09-30 21:46:51 +0200

    Fix backward compatibility.

--- src/main/java/hudson/plugins/disk_usage/DiskUsageProperty.java
+++ src/main/java/hudson/plugins/disk_usage/DiskUsageProperty.java
@@ -152,6 +152,8 @@ public class DiskUsageProperty extends JobProperty<Job<?, ?>> {
         }
         
         public Long getDiskUsageWithoutBuilds(){
+            if(diskUsageWithoutBuilds==null)
+                diskUsageWithoutBuilds=0l;
             return diskUsageWithoutBuilds;
         }
         
