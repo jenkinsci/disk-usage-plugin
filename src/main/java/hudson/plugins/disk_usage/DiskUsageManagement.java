454be7a46fd59418acfa9e5ab7386cab436ba42d
lvotypko <lvotypko@redhat.com>
2013-10-27 09:23:08 +0100

    Fix link in menu on main page.

--- src/main/java/hudson/plugins/disk_usage/DiskUsageManagement.java
+++ src/main/java/hudson/plugins/disk_usage/DiskUsageManagement.java
@@ -7,6 +7,11 @@ package hudson.plugins.disk_usage;
 import hudson.Extension;
 import hudson.model.ManagementLink;
 import hudson.model.RootAction;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import jenkins.model.Jenkins;
+import org.kohsuke.stapler.StaplerRequest;
+import org.kohsuke.stapler.StaplerResponse;
 
 /**
  *
@@ -26,11 +31,16 @@ public class DiskUsageManagement extends ManagementLink implements RootAction{
         }
 
         public String getUrlName() {
-            return "plugin/disk-usage/";
+            return "disk-usage";
         }
 
         @Override public String getDescription() {
             return Messages.Description();
-        }     
+        }  
+        
+        public void doIndex(StaplerRequest req, StaplerResponse res) throws ServletException, IOException{
+            DiskUsagePlugin plugin = Jenkins.getInstance().getPlugin(DiskUsagePlugin.class);
+            res.sendRedirect(Jenkins.getInstance().getRootUrl() + "plugin/disk-usage");
+        }
     
 }
